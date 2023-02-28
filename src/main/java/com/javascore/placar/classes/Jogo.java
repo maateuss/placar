c6926107b88b8c81e54f333b09a4994e283c9f26
Mateus Moura Santos <msantos.96@outlook.com>
2023-02-28 18:18:51 -0300

    Criacao da service de jogos

---
+++ src/main/java/com/javascore/placar/classes/Jogo.java
@@ -0,0 +1,28 @@
+package com.javascore.placar.classes;
+
+import java.util.List;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Jogo {
+    @Builder.Default
+    private UUID id = UUID.randomUUID();
+    private String timeCasa;
+    private String timeVisitante;
+    private int golsTimeCasa;
+    private int golsTimeVisitante;
+    private List<String> eventos;
+
+    public Jogo removerIdentificador() {
+        this.id = null;
+        return this;
+    }
+}
---
+++ src/main/java/com/javascore/placar/services/JogoService.java
@@ -0,0 +1,64 @@
+package com.javascore.placar.services;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Service;
+
+import com.javascore.placar.classes.Jogo;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+@Service
+public class JogoService {
+    private final Sinks.Many<Jogo> jogosSink = Sinks.many().multicast().onBackpressureBuffer();
+    private final Map<UUID, Jogo> jogosMap = new ConcurrentHashMap<>();
+
+    
+    public void adicionarJogo(Jogo novoJogo){
+        jogosMap.put(novoJogo.getId(), novoJogo);
+        jogosSink.tryEmitNext(novoJogo);
+    }
+
+    public Flux<Jogo> getJogosFlux(){
+        return jogosSink.asFlux();
+    }
+
+    public Mono<Jogo> getJogo(UUID id) {
+        return Mono.justOrEmpty(jogosMap.get(id));
+    }
+
+    public Flux<Jogo> getJogos() {
+        return Flux.fromIterable(jogosMap.values());
+    }
+
+    public Mono<Jogo> atualizarJogo(UUID id, Jogo jogoAtualizado) {
+        return Mono.fromCallable(() -> {
+            Jogo jogoExistente = jogosMap.get(id);
+            if (jogoExistente == null) {
+                throw new Exception("Jogo não encontrado");
+            }
+            jogoAtualizado.setId(jogoExistente.getId());
+            jogosMap.put(jogoAtualizado.getId(), jogoAtualizado);
+            jogosSink.tryEmitNext(jogoAtualizado);
+            return jogoAtualizado;
+        });
+    }
+
+    public Mono<Void> removerJogo(UUID id) {
+        return Mono.fromCallable(() -> {
+            Jogo jogoExistente = jogosMap.get(id);
+            if (jogoExistente == null) {
+                throw new Exception("Jogo não encontrado");
+            }
+            jogosMap.remove(id);
+            jogosSink.tryEmitNext(jogoExistente.removerIdentificador());
+            return null;
+        });
+    }
+    
+
+}
